060c1bbe28942c10c9fbac0483eb01d67928cc6f
harsh1504660 <harshsmj1504@gmail.com>
2025-07-06 02:04:50 +0530

    added allow credentials false

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ sessions: Dict[str, List] = {}
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*","http://localhost:8080"],  # or specify your frontend origin
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
