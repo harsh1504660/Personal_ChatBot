dd9e58211a01589af46ff9cd4319fd175258ddcf
Harsh Joshi <77767961+harsh1504660@users.noreply.github.com>
2025-07-06 16:36:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 chunks = text_splitter.split_documents(docs)
 
 embedding_model = CohereEmbeddings(
-    cohere_api_key=os.environ.get("cohere"),
+    cohere_api_key='4yKUbbNxAHfezeuyc9eCJBtKmsZVJ2Oahcv945xW',
     user_agent="langchain"
 )
 
